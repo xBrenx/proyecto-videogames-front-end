5ee0af7fbe9108a2e5870f091edd7142aa773708
xBrenx <brenda_panes@hotmail.com>
2022-12-06 15:29:24 -0300

    notfound

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import {Route} from 'react-router-dom';
-import React from 'react';
-import './App.css';
-import Nav from './Pages/Nav/Nav';
-import Home from './Pages/Home/Home'
-import start from './Pages/Start/start';
-import Details from './Pages/Details/Details';
-import Create from './Pages/Create/Create';
-
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Nav from "./Pages/Nav/Nav";
+import Home from "./Pages/Home/Home";
+import start from "./Pages/Start/start";
+import Details from "./Pages/Details/Details";
+import Create from "./Pages/Create/Create";
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/" component={start} />
 
-      <Route exact path="/" component={ start }/>
+        <Route exact path="/videogames">
+          <Nav />
+          <Home />
+        </Route>
 
-      <Route exact path="/videogames">
-        <Nav/>
-        <Home/>
-      </Route>
-      
-      <Route exact path="/create">
-        <Create/>
-      </Route>
-      
-      <Route exact path="/videogames/:id">
-        <Details/>
-      </Route>
-      
+        <Route exact path="/create">
+          <Create />
+        </Route>
+
+        <Route exact path="/videogames/:id">
+          <Details />
+        </Route>
+
+        <Route path="*">
+          <h2>Pagina no encontrada.</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
